581410a29225b2815c88b3c09e351687acebc819
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-05-21 22:56:49 +0300

    what is echo

---
+++ secondLeson/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?> 
+
+$x = 5 /* + 15 */ + 5;
+echo $x;
+</body>
+</html>
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
